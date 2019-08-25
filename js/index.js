65f60691f41148c1496d4093aa13fee40102ed60
jay-68 <kelvinngari1995@gmail.com>
2019-08-25 19:51:00 +0300

    Add function that toggles between image and content in the what we do section

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('#design').click(function() {
+  $('#design-img').toggle();
+  $('#design-content').toggle();
+});
+
+$('#development').click(function() {
+  $('#dev-img').toggle();
+  $('#dev-content').toggle();
+});
+$('#product').click(function() {
+  $('#product-img').toggle();
+  $('#product-content').toggle();
+});
