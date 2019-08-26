227543e414fdce10835c0a832581bdb4dc536672
jay-68 <kelvinngari1995@gmail.com>
2019-08-26 10:33:31 +0300

    Add hover effect on the images in the portfolio section

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-$('#design').click(function() {
+$('#design').click(function () {
   $('#design-img').toggle();
   $('#design-content').toggle();
 });
-
-$('#development').click(function() {
+$('#development').click(function () {
   $('#dev-img').toggle();
   $('#dev-content').toggle();
 });
-$('#product').click(function() {
+$('#product').click(function () {
   $('#product-img').toggle();
   $('#product-content').toggle();
 });
+
+$('#image1').hover(function () {
+  $('.title1').toggle(100);
+})
+$('#image2').hover(function () {
+  $('.title2').toggle(100);
+})
+$('#image3').hover(function () {
+  $('.title3').toggle(100);
+})
+$('#image4').hover(function () {
+  $('.title4').toggle(100);
+})
+$('#image5').hover(function () {
+  $('.title5').toggle(100);
+})
+$('#image6').hover(function () {
+  $('.title6').toggle(100);
+})
+$('#image7').hover(function () {
+  $('.title7').toggle(100);
+})
+$('#image8').hover(function () {
+  $('.title8').toggle(100);
+})
+
+let btn = document.getElementById("submitButton");
+
